5e74040ad3a4cfbc823df3c861445aa299045639
dwisyafriadi2 <dwisyafriadi15@gmail.com>
2025-01-28 20:58:56 +0700

    Update BOT v1, V2, Select Model

--- autoinstall.sh
+++ autoinstall.sh
@@ -113,6 +113,7 @@ auto_interaction_v2() {
     python3 -m venv env
     source env/bin/activate
     pip install -r "$HOME/gaianet-node/requirements.txt"
+    pip3 install -r "$HOME/gaianet-node/requirements.txt"
     python3 "$script_path" > "$log_file" 2>&1 &
     echo $! > "$pid_file"
 
@@ -153,6 +154,7 @@ check_python_version() {
             echo "Installing python3-venv package..."
             sudo apt update
             sudo apt install -y python3-venv
+            sudo apt install python3-pip
         fi
     else
         echo "Python 3 is not installed. Installing Python 3..."
